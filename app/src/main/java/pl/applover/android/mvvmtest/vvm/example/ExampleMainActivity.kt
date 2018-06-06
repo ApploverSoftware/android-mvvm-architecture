a5b8aa74fc03a20a3afc7181102e751d275e935c
Janusz <januszhain@wp.pl>
2018-06-06 16:06:36 +0200

    Fixed ViewModels not getting "OnClear"

--- app/src/main/java/pl/applover/android/mvvmtest/vvm/example/ExampleMainActivity.kt
+++ app/src/main/java/pl/applover/android/mvvmtest/vvm/example/ExampleMainActivity.kt
@@ -1,26 +1,26 @@
 package pl.applover.android.mvvmtest.vvm.example
 
 import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import kotlinx.android.synthetic.main.example_activity_main.*
 import pl.applover.android.mvvmtest.R
 import pl.applover.android.mvvmtest.util.extensions.goToActivity
 import pl.applover.android.mvvmtest.util.other.DaggerAppCompatActivity
 import pl.applover.android.mvvmtest.vvm.example.next_example.NextExampleActivity
-import javax.inject.Inject
 
 
 class ExampleMainActivity : DaggerAppCompatActivity() {
 
 
-    @Inject
     internal lateinit var viewModel: ExampleMainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.example_activity_main)
+        viewModel = ViewModelProviders.of(this).get(ExampleMainViewModel()::class.java)
         viewModel.someEvent.observe(this, Observer { event -> println(event?.getContentIfNotHandled()) })
-        viewModel.title.observe(this, Observer { title -> textViewHelloWorld.text = title})
+        viewModel.title.observe(this, Observer { title -> textViewHelloWorld.text = title })
 
         textViewHelloWorld.setOnClickListener {
             println("Go to next Activity")
@@ -36,4 +36,5 @@ class ExampleMainActivity : DaggerAppCompatActivity() {
         super.onResume()
         viewModel.activityOnResume()
     }
+
 }
--- app/src/main/java/pl/applover/android/mvvmtest/vvm/example/next_example/NextExampleActivity.kt
+++ app/src/main/java/pl/applover/android/mvvmtest/vvm/example/next_example/NextExampleActivity.kt
@@ -1,37 +1,33 @@
 package pl.applover.android.mvvmtest.vvm.example.next_example
 
 import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import kotlinx.android.synthetic.main.example_activity_main.*
 import pl.applover.android.mvvmtest.R
-import pl.applover.android.mvvmtest.util.extensions.goToActivity
 import pl.applover.android.mvvmtest.util.other.DaggerAppCompatActivity
 import javax.inject.Inject
 
 
 class NextExampleActivity : DaggerAppCompatActivity() {
 
-
-    @Inject
     internal lateinit var viewModel: NextExampleViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.next_example_activity_main)
+        viewModel = ViewModelProviders.of(this).get(NextExampleViewModel()::class.java)
         viewModel.someEvent.observe(this, Observer { event -> println(event?.getContentIfNotHandled()) })
         viewModel.title.observe(this, Observer { title -> textViewHelloWorld.text = title })
-        textViewHelloWorld.setOnClickListener {
-            println("Go to next Activity")
-            goToNextActivity()
-        }
-    }
-
-    private fun goToNextActivity() {
-        goToActivity(NextExampleActivity::class.java, true)
     }
 
     override fun onResume() {
         super.onResume()
         viewModel.activityOnResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("OnDestroy")
     }
 }
