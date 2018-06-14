8dfe60ef52fa36cd980d888e81576c9ebf3da40e
Janusz <januszhain@wp.pl>
2018-06-14 14:39:31 +0200

    Added new utils and method of getting Single to load cities

--- app/src/main/java/pl/applover/android/mvvmtest/data/example/repositories/ExampleCitiesRepository.kt
+++ app/src/main/java/pl/applover/android/mvvmtest/data/example/repositories/ExampleCitiesRepository.kt
@@ -1,12 +1,19 @@
 package pl.applover.android.mvvmtest.data.example.repositories
 
+import io.reactivex.Scheduler
 import pl.applover.android.mvvmtest.data.example.internet.api_endpoints.ExampleCitiesApiEndpointsInterface
+import pl.applover.android.mvvmtest.util.other.MyScheduler
 import javax.inject.Inject
-import javax.inject.Named
 
 /**
  * Created by Janusz Hain on 2018-06-14.
  */
-class ExampleCitiesRepository @Inject constructor(val api: ExampleCitiesApiEndpointsInterface) {
+class ExampleCitiesRepository @Inject constructor(private val api: ExampleCitiesApiEndpointsInterface) {
+
+    fun loadCitiesFromNetwork(observeOn: Scheduler = MyScheduler.getMainThreadScheduler(),
+                              subscribeOn: Scheduler = MyScheduler.getScheduler()) =
+            api.getCitiesList()
+                    .subscribeOn(subscribeOn)
+                    .observeOn(observeOn)
 
 }
