db2f75eaf37808da3cf94cad13f9089d12997132
Janusz <januszhain@wp.pl>
2018-06-18 07:24:57 +0200

    Refactoring

--- app/src/main/java/pl/applover/android/mvvmtest/vvm/example/next_example/example_list/ExampleListViewModelFactory.kt
+++ app/src/main/java/pl/applover/android/mvvmtest/vvm/example/next_example/example_list/ExampleListViewModelFactory.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 /**
  * Created by Janusz Hain on 2018-06-06.
  */
-class ExampleListViewModelFactory @Inject constructor(val exampleListFragmentRouter: ExampleListFragmentRouter) : ViewModelProvider.Factory {
+class ExampleListViewModelFactory @Inject constructor(private val exampleListFragmentRouter: ExampleListFragmentRouter) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
