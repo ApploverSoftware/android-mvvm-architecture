d3cc4115ebca6e080f064d1ecf76aadd6bcf67d4
Janusz <januszhain@wp.pl>
2018-06-18 11:59:18 +0200

    Added new functions to MappedResponse

--- app/src/main/java/pl/applover/android/mvvmtest/util/architecture/retrofit/MappedResponse.kt
+++ app/src/main/java/pl/applover/android/mvvmtest/util/architecture/retrofit/MappedResponse.kt
@@ -1,10 +1,47 @@
 package pl.applover.android.mvvmtest.util.architecture.retrofit
 
+import okhttp3.Headers
 import okhttp3.ResponseBody
 
 /**
  * Created by Janusz Hain on 2018-06-18.
  */
 
-data class MappedResponse<T>(val rawResponse: okhttp3.Response, val body: T?,
-                          val errorBody: ResponseBody?)
+class MappedResponse<T>(private val rawResponse: okhttp3.Response,
+                             private val body: T?,
+                             private val errorBody: ResponseBody?) {
+
+    /** HTTP status code.  */
+    fun code(): Int {
+        return rawResponse.code()
+    }
+
+    /** HTTP status message or null if unknown.  */
+    fun message(): String {
+        return rawResponse.message()
+    }
+
+    /** HTTP headers.  */
+    fun headers(): Headers {
+        return rawResponse.headers()
+    }
+
+    /** Returns true if [.code] is in the range [200..300).  */
+    fun isSuccessful(): Boolean {
+        return rawResponse.isSuccessful
+    }
+
+    /** The deserialized response body of a [successful][.isSuccessful] response.  */
+    fun body(): T? {
+        return body
+    }
+
+    /** The raw response body of an [unsuccessful][.isSuccessful] response.  */
+    fun errorBody(): ResponseBody? {
+        return errorBody
+    }
+
+    override fun toString(): String {
+        return rawResponse.toString()
+    }
+}
