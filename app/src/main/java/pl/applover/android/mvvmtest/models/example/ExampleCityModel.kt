458a5f62d65b9b3659e3ff76df0ba7bbe3bdaf54
Janusz <januszhain@wp.pl>
2018-06-21 09:15:08 +0200

    Added data binding example for city model

--- app/src/main/java/pl/applover/android/mvvmtest/models/example/ExampleCityModel.kt
+++ app/src/main/java/pl/applover/android/mvvmtest/models/example/ExampleCityModel.kt
@@ -1,6 +1,8 @@
 package pl.applover.android.mvvmtest.models.example
 
 import android.databinding.BaseObservable
+import android.databinding.Bindable
+import com.android.databinding.library.baseAdapters.BR
 import pl.applover.android.mvvmtest.data.example.database.models.ExampleCityDbModel
 import pl.applover.android.mvvmtest.data.example.internet.response.ExampleCityResponse
 
@@ -10,8 +12,17 @@ class ExampleCityModel(
         val name: String,
         val lat: Double,
         val lng: Double,
-        val country: String
-): BaseObservable() {
+        val country: String,
+        private var _numberOfClicks: Int? = null
+) : BaseObservable() {
+
+    var numberOfClicks: Int?
+        @Bindable get() = _numberOfClicks
+        set(value) {
+            _numberOfClicks = numberOfClicks
+            notifyPropertyChanged(BR.numberOfClicks)
+        }
+
 
     constructor(exampleCityResponse: ExampleCityResponse) : this(
             exampleCityResponse.id,
