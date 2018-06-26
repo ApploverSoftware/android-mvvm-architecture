5e463049d673721d46c7f08764ad0e8a948bc017
Janusz <januszhain@wp.pl>
2018-06-26 15:44:45 +0200

    Added working test for router sending

--- app/src/test/java/pl/applover/android/mvvmtest/vm/fragments/ExampleListViewModelUnitTest.kt
+++ app/src/test/java/pl/applover/android/mvvmtest/vm/fragments/ExampleListViewModelUnitTest.kt
@@ -1,8 +1,20 @@
 package pl.applover.android.mvvmtest.vm.fragments
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import io.reactivex.subjects.PublishSubject
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import pl.applover.android.mvvmtest.data.example.repositories.ExampleCitiesRepository
+import pl.applover.android.mvvmtest.util.architecture.rx.EmptyEvent
+import pl.applover.android.mvvmtest.vvm.example.nextExample.exampleList.ExampleListFragmentRouter
+import pl.applover.android.mvvmtest.vvm.example.nextExample.exampleList.ExampleListViewModel
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +22,42 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleListViewModelUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var mockRepository: ExampleCitiesRepository
+
+    private lateinit var exampleListViewModel: ExampleListViewModel
+
+    private val router = ExampleListFragmentRouter()
+
+    @Mock
+    private lateinit var mockedPublishSubject: PublishSubject<EmptyEvent>
+
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        val field = ExampleListFragmentRouter.Sender::class.java.getDeclaredField("fragmentClicked")
+        field.isAccessible = true
+        val modifiersField = Field::class.java.getDeclaredField("modifiers")
+        modifiersField.isAccessible = true
+        modifiersField.setInt(field, field.modifiers and Modifier.FINAL.inv())
+
+        field.set(router.sender, mockedPublishSubject)
+
+        println(router.sender.fragmentClicked)
+        exampleListViewModel = ExampleListViewModel(router, mockRepository)
     }
+
+    @Test
+    fun testRouterSender() {
+        exampleListViewModel.fragmentClicked()
+        Mockito.verify(router.sender.fragmentClicked, Mockito.times(1)).onNext(Mockito.any())
+    }
+
+
 }
