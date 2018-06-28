af6bfffe619411060b8486499d10641543940a5e
Janusz <januszhain@wp.pl>
2018-06-28 10:46:39 +0200

    Added common test package

--- app/src/test/java/pl/applover/android/mvvmtest/ExampleUnitTest.kt
+++ app/src/test/java/pl/applover/android/mvvmtest/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package pl.applover.android.mvvmtest
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
