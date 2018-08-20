9201d3a10a1a561a897b8687df8fdd9a57026689
Janusz <januszhain@wp.pl>
2018-08-20 15:12:43 +0200

    Added observable delegate

--- app/src/main/java/pl/applover/android/mvvmtest/models/example/ExampleCityModel.kt
+++ app/src/main/java/pl/applover/android/mvvmtest/models/example/ExampleCityModel.kt
@@ -1,17 +1,24 @@
 package pl.applover.android.mvvmtest.models.example
 
+import android.annotation.SuppressLint
 import android.databinding.BaseObservable
 import android.databinding.Bindable
+import android.os.Parcelable
 import com.android.databinding.library.baseAdapters.BR
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
 import pl.applover.android.mvvmtest.data.example.database.models.ExampleCityDbModel
 import pl.applover.android.mvvmtest.data.example.internet.response.ExampleCityResponse
+import kotlin.properties.Delegates
 
 
 /**
  * Model for DataBinding
  * Notice that vars have to be built differently as var's set will have to notify about property being changed
  */
-class ExampleCityModel(
+@SuppressLint("ParcelCreator")
+@Parcelize
+data class ExampleCityModel(
         @get:Bindable
         val id: String,
         @get:Bindable
@@ -22,16 +29,16 @@ class ExampleCityModel(
         val lng: Double,
         @get:Bindable
         val country: String,
-        private var _numberOfClicks: Int = 0
-) : BaseObservable() {
+        private var _numberOfClicks: Int = 0 //needed to save number of clicks for Parcelable
+) : BaseObservable(), Parcelable {
 
-    var numberOfClicks: Int
-        @Bindable get() = _numberOfClicks
-        set(value) {
-            _numberOfClicks = value
-            notifyPropertyChanged(BR.numberOfClicks)
-        }
-
+    @IgnoredOnParcel
+    @get:Bindable
+    @delegate:Transient
+    var numberOfClicks by Delegates.observable(_numberOfClicks) { _, _, value ->
+        _numberOfClicks = value
+        notifyPropertyChanged(BR.numberOfClicks)
+    }
 
     constructor(exampleCityResponse: ExampleCityResponse) : this(
             exampleCityResponse.id,
