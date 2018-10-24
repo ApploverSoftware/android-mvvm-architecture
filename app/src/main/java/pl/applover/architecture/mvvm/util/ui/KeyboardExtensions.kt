df9b47010c6614e259f0e4d99d43e8250375b66e
Janusz <januszhain@wp.pl>
2018-10-24 11:01:24 +0200

    Changed toggle to use reflection to detect whether keyboard is visible or not

--- app/src/main/java/pl/applover/architecture/mvvm/util/ui/KeyboardExtensions.kt
+++ app/src/main/java/pl/applover/architecture/mvvm/util/ui/KeyboardExtensions.kt
@@ -4,18 +4,34 @@ import android.content.Context
 import android.support.v4.app.Fragment
 import android.view.inputmethod.InputMethodManager
 import pl.applover.architecture.mvvm.App
+import timber.log.Timber
 
 
 /**
  * Created by Janusz Hain on 2018-03-07.
  */
 
-fun Fragment.hideKeyboard(context: Context = App.instance) {
+/**
+ * If no window token is found, keyboard is checked using reflection to know if keyboard visibility toggle is needed
+ *
+ * @param useReflection - whether to use reflection in case of no window token or not
+ */
+fun Fragment.hideKeyboard(context: Context = App.instance, useReflection: Boolean = true) {
     val windowToken = view?.rootView?.windowToken
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     windowToken?.let {
         imm.hideSoftInputFromWindow(windowToken, 0)
     } ?: run {
-        imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
+        if (useReflection) {
+            try {
+                if (getKeyboardHeight(imm) > 0) {
+                    imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
+                }
+            } catch (exception: Exception) {
+                Timber.e(exception)
+            }
+        }
     }
 }
+
+fun getKeyboardHeight(imm: InputMethodManager): Int = InputMethodManager::class.java.getMethod("getInputMethodWindowVisibleHeight").invoke(imm) as Int
